e9359433b3a37fc8a6b929c32c04f7d8a2bba69e
MoustafaEbada <st112379@stud.uni-stuttgart.de>
2025-02-27 14:09:31 +0000

    Fix GitHub Pages deployment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return <h1>{message}</h1>;
+}
+export default App;
